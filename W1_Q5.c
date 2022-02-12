4419285bc3e6d80eda146fc817abf878fa56dba1
Sugyani Manjari Dash <99403465+Sugyani31@users.noreply.github.com>
2022-02-12 05:43:59 -0800

    Update W1_Q5.c

--- W1_Q5.c
+++ W1_Q5.c
@@ -3,11 +3,20 @@
 int main()
 {
     float r,d,a,c;
+    
+    /*enter radius*/
+    
     printf("enter the radius of circle\n");
     scanf("%f", &r);
+    
+    /*calculation of diameter,area and circumference*/
+    
     d=(2*r);
     a=(3.14*r*r);
     c=(2*3.14*r);
+    
+    /*display diameter,area and circumference of circle*/
+    
     printf("the diameter of the circle is %f\n", d);
     printf("the area of the circle is %f\n", a);
      printf("the circumference of the circle is %f\n", c);
